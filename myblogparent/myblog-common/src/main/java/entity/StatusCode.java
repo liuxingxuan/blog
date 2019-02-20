e33875b5a9d7325fb18ae54a4331bde588d44873
lxx <853555374@qq.com>
2019-02-20 09:47:33 +0800

    fix StatusCode

--- myblogparent/myblog-common/src/main/java/entity/StatusCode.java
+++ myblogparent/myblog-common/src/main/java/entity/StatusCode.java
@@ -12,4 +12,30 @@ public class StatusCode {
     public static final int REMOTEERROR =20004;//远程调用失败
     public static final int REPERROR =20005;//重复操作
 
+    public StatusCode() {
+    }
+
+    public static int getOK() {
+        return OK;
+    }
+
+    public static int getERROR() {
+        return ERROR;
+    }
+
+    public static int getLOGINERROR() {
+        return LOGINERROR;
+    }
+
+    public static int getACCESSERROR() {
+        return ACCESSERROR;
+    }
+
+    public static int getREMOTEERROR() {
+        return REMOTEERROR;
+    }
+
+    public static int getREPERROR() {
+        return REPERROR;
+    }
 }
